e2c35ecf1981311df521ef47878798fe40a3ed64
botchway44 <noelnuel44@gmail.com>
2021-05-29 20:00:16 +0000

    removed build from cache

--- dist/dto/response.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const allInvocationSource = ["DialogCodeHook", "FulfillmentCodeHook"];
-const allInputMode = ["DTMF", "Speech", "Text"];
-const allConfirmationState = ["Confirmed", "Denied", "None"];
-const allResponseContentType = ["CustomPayload ", " ImageResponseCard ", " PlainText ", " SSML"];
-// export type ResponseContentType = "CustomPayload " | " ImageResponseCard " | " PlainText " | " SSML";
-const allDialogActionType = ["Close ", " ConfirmIntent ", " Delegate ", " ElicitIntent ", " ElicitSlot"];
-const allStates = ["Failed", "Fulfilled", "InProgress", "ReadyForFulfillment"];
--- dist/utils/response-utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCloseResponseDTO = exports.createDialogAction = exports.createIntent = exports.createMessage = void 0;
-const dto_1 = require("../dto");
-exports.createMessage = (message, type) => new dto_1.Message(message, type);
-exports.createIntent = (name, confirmationState, slots, state) => new dto_1.CreateIntent(name, confirmationState, slots, state);
-exports.createDialogAction = (type) => new dto_1.DialogAction(type);
-exports.createCloseResponseDTO = (sessionAttributes, slots, intentName, state, confirmationState, message) => new dto_1.CloseResquestResponse(new dto_1.SessionState(sessionAttributes, exports.createDialogAction("Close"), exports.createIntent(intentName, confirmationState, slots, state)), new dto_1.MessageBuilder(new dto_1.Message(message)).allMessages());
